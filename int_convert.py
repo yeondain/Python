325033391293536a27c1d28e7fb7b7b581427cb1
yeondain <cksek27@daum.net>
2022-10-18 16:57:15 +0900

    2022.10.18

---
+++ int_convert.py
@@ -0,0 +1,14 @@
+String_a = input("입력 : ")
+int_a = int(String_a)
+
+String_b = input("입력 : ")
+int_b = int(String_b)
+
+print("문자열 자료 :",String_a + String_b)
+print("숫자 자료 : ", int_a + int_b)
+
+#input()함수의 입력 자료형은 항상 문자열이기 때문에 문자열을 숫자로 변환해야 숫자 연산에 활용 가능
+#영어로 캐스트라고 함
+#int() 사용하여 문자열을 -> 숫자로
+
+
---
+++ int_float.py
@@ -0,0 +1,12 @@
+output_A = int("52")
+output_B = float("52.235")
+print(type(output_A),output_A)
+print(type(output_B),output_B)
+
+intput_a = float(input("첫번째 수 : "))
+intput_b = int(input("두번째 수 : "))
+
+print("덧셈결과 : ", intput_a + intput_b)
+print("뺄셈결과 : ", intput_a - intput_b)
+print("곱셈결과 : ", intput_a * intput_b)
+print("나누기결과 : ", intput_a / intput_b)
