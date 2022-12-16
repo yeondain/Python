373f20b4a7d66eed9bb4beedf358c3771a01edde
yeondain <cksek27@daum.net>
2022-12-16 15:53:26 +0900

    이래도

---
+++ ouput.py
@@ -0,0 +1,14 @@
+print("#하나만 출력")
+print("Hello Python Programming")
+print()
+
+print("#여러개 출력")
+print(10,20,30,40)
+print("안녕","다인이야")
+print()
+
+print("#아무것도 출력안하기")
+print("----")
+print()
+print("----")
+print ()
